c91220ad17e69f1a8e89b9c35d62d9f7fbba9ae0
Konstantin.Tarasov <konstantin.tarasov@nyct.com>
2025-04-25 16:34:48 -0400

    added color, lights, materials and poi + surface normal

--- src/lib.rs
+++ src/lib.rs
@@ -2,102 +2,110 @@ use core::f32;
 
 pub struct Vector(pub f32, pub f32, pub f32);
 
-    // Implemeting functions for the Vector struct
-    impl Vector{
-        pub fn sub(&self, vector:&Vector) -> Vector{
-            Vector(
-                self.0 - vector.0,
-                self.1 - vector.1,
-                self.2 - vector.2
-            )
-        }
-
-        pub fn add(&self, vector:&Vector) -> Vector{
-            Vector(
-                self.0 + vector.0,
-                self.1 + vector.1,
-                self.2 + vector.2
-            )
-        }
-
-        pub fn scale(&self, scalar: f32) -> Vector{
-            Vector(
-                self.0 * scalar,
-                self.1 * scalar, 
-                self.2 * scalar)
-        }
-
-        pub fn norm(&self) -> Vector {
-            let len = self.len();
-            if len == 0.0 {
-                panic!("Tried to normalize a zero-length vector");
-            }
-            self.scale(1.0 / len)
-        }
-
-        pub fn dot_prod(&self, vector: &Vector) -> f32{
-            self.0 * vector.0 + self.1 * vector.1 + self.2 * vector.2
-        }
-
-        pub fn lerp(&self, vector:&Vector, alpha: f32) -> Vector{
-            // Linear Extrapolation
-            self.scale(1.0-alpha).add(&vector.scale(alpha))
-        }
-
-        pub fn len(&self) -> f32 {
-            (self.0.powi(2) + self.1.powi(2) + self.2.powi(2)).sqrt()
-        }
-
-        pub fn to_rgb(&self) -> [u8; 3] {
-            [
-                ((self.0.clamp(-1.0, 1.0) * 0.5 + 0.5) * 255.0) as u8,
-                ((self.1.clamp(-1.0, 1.0) * 0.5 + 0.5) * 255.0) as u8,
-                // ((self.2.clamp(-1.0, 1.0) * 0.5 + 0.5) * 255.0) as u8,
-                127
-            ]
-        }
-
-    
+// Implemeting functions for the Vector struct
+impl Vector {
+    pub fn sub(&self, vector: &Vector) -> Vector {
+        Vector(self.0 - vector.0, self.1 - vector.1, self.2 - vector.2)
     }
 
-pub struct Sphere{
+    pub fn add(&self, vector: &Vector) -> Vector {
+        Vector(self.0 + vector.0, self.1 + vector.1, self.2 + vector.2)
+    }
+
+    pub fn scale(&self, scalar: f32) -> Vector {
+        Vector(self.0 * scalar, self.1 * scalar, self.2 * scalar)
+    }
+
+    pub fn norm(&self) -> Vector {
+        let len = self.len();
+        if len == 0.0 {
+            panic!("Tried to normalize a zero-length vector");
+        }
+        self.scale(1.0 / len)
+    }
+
+    pub fn dot_prod(&self, vector: &Vector) -> f32 {
+        self.0 * vector.0 + self.1 * vector.1 + self.2 * vector.2
+    }
+
+    pub fn lerp(&self, vector: &Vector, alpha: f32) -> Vector {
+        // Linear Extrapolation
+        self.scale(1.0 - alpha).add(&vector.scale(alpha))
+    }
+
+    pub fn len(&self) -> f32 {
+        (self.0.powi(2) + self.1.powi(2) + self.2.powi(2)).sqrt()
+    }
+
+    pub fn to_rgb(&self) -> [u8; 3] {
+        [
+            ((self.0.clamp(-1.0, 1.0) * 0.5 + 0.5) * 255.0) as u8,
+            ((self.1.clamp(-1.0, 1.0) * 0.5 + 0.5) * 255.0) as u8,
+            // ((self.2.clamp(-1.0, 1.0) * 0.5 + 0.5) * 255.0) as u8,
+            127,
+        ]
+    }
+}
+
+pub struct Color(pub f32, pub f32, pub f32);
+
+pub struct Sphere {
     pub center: Vector,
     pub radius: f32,
-    pub color: (f32, f32, f32), // needs to be 0.0..1.0
+    pub color: Color, // needs to be 0.0..1.0
+    pub material: Material
 }
 
-pub fn intersection_test(direction: &Vector, 
-    sphere: &Sphere, 
-    origin: &Vector) -> f32 {
-
-let c_prime = origin.sub(&sphere.center);
-
-let a = direction.dot_prod(&direction);
-let b = 2.0 * c_prime.dot_prod(&direction);
-let c = c_prime.len().powi(2) - sphere.radius.powi(2);
-
-let discriminant = b.powi(2) - 4.0 * a * c;
-
-match discriminant.is_sign_positive() {
-false => return 0.0,
-true => discriminant
-};
-
-let t_plus = (-b + discriminant.sqrt()) / (2.0*a);
-let t_plus = match t_plus > 0.0 {
-true => t_plus,
-false => f32::NAN
-};
-let t_minus = (-b - discriminant.sqrt()) / (2.0*a);
-let t_minus = match t_minus > 0.0 {
-true => t_minus,
-false => f32::NAN
-};
-let t = t_plus.min(t_minus);
-
-match t.is_nan() {
-true => 0.0,
-false => t
+impl Sphere{
+    pub fn intersection_point(&self, origin: &Vector, d: &Vector, t: f32) -> Vector{
+        origin.add(&d.scale(t))
+    }
+    pub fn surf_normal(&self, p: &Vector) -> Vector {
+        p.sub(&self.center).norm()
+    }
 }
 
+pub fn intersection_test(direction: &Vector, sphere: &Sphere, origin: &Vector) -> f32 {
+    let c_prime = origin.sub(&sphere.center);
+
+    let a = direction.dot_prod(&direction);
+    let b = 2.0 * c_prime.dot_prod(&direction);
+    let c = c_prime.len().powi(2) - sphere.radius.powi(2);
+
+    let discriminant = b.powi(2) - 4.0 * a * c;
+
+    match discriminant.is_sign_positive() {
+        false => return 0.0,
+        true => discriminant,
+    };
+
+    let t_plus = (-b + discriminant.sqrt()) / (2.0 * a);
+    let t_plus = match t_plus > 0.0 {
+        true => t_plus,
+        false => f32::NAN,
+    };
+    let t_minus = (-b - discriminant.sqrt()) / (2.0 * a);
+    let t_minus = match t_minus > 0.0 {
+        true => t_minus,
+        false => f32::NAN,
+    };
+    let t = t_plus.min(t_minus);
+
+    match t.is_nan() {
+        true => 0.0,
+        false => t,
+    }
+}
+
+pub struct Light {
+    pub location: Vector,
+    pub diffuse_int: Color,  // needs to be 0.0..1.0
+    pub specular_int: Color, // needs to be 0.0..1.0
+}
+
+pub struct Material {
+    pub ambient_k: Color,
+    pub diffuse_k: f32,
+    pub specular_k: f32,
+    pub shininess: i32
 }
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,87 @@
 use image::{Rgb, RgbImage};
-use ray_tracing::{Vector, Sphere, intersection_test};
+use ray_tracing::{intersection_test, Color, Light, Material, Sphere, Vector};
 
 fn main() {
     // Image plain corners vectors
     let x1 = Vector(1.0, 0.75, 0.0); //Top right
     let x2 = Vector(-1.0, 0.75, 0.0); //Top left
     let x3 = Vector(1.0, -0.75, 0.0); // Bottom right
-    let x4= Vector(-1.0, -0.75, 0.0); //Bottom left
+    let x4 = Vector(-1.0, -0.75, 0.0); //Bottom left
 
     // Camera vector
-    let c = Vector(0.0,0.0,-1.0);
+    let c = Vector(0.0, 0.0, -1.0);
 
     // Plain dimensions in pixels
     let width = 256;
     let height = 192;
 
-
-    
-    let sphere1 = Sphere{
+    let sphere1 = Sphere {
         center: Vector(0.2, -0.2, 2.0),
         radius: 1.0,
-        color: (1.0,0.0,0.0) // Red
+        color: Color(1.0, 0.0, 0.0), // Red
+        material: Material{
+            ambient_k: Color(0.2, 0.2, 0.2),
+            diffuse_k: 0.5,
+            specular_k: 0.3,
+            shininess: 3
+        }
     };
 
-    let sphere2 = Sphere{
+    let sphere2 = Sphere {
         center: Vector(-0.9, 0.5, 4.0),
         radius: 2.0,
-        color: (0.0,1.0,0.0) // Green
+        color: Color(0.0, 1.0, 0.0), // Green
+        material: Material{
+            ambient_k: Color(0.5, 0.3, 0.0),
+            diffuse_k: 0.2,
+            specular_k: 0.1,
+            shininess: 2
+        }
     };
 
-    let sphere3 = Sphere{
+    let sphere3 = Sphere {
         center: Vector(-0.75, 0.6, 3.0),
         radius: 1.2,
-        color: (0.0,0.0,1.0) // Blue
+        color: Color(0.0, 0.0, 1.0), // Blue
+        material: Material{
+            ambient_k: Color(0.0, 0.9, 0.7),
+            diffuse_k: 0.8,
+            specular_k: 0.9,
+            shininess: 6
+        }
     };
 
     let spheres = vec![sphere1, sphere2, sphere3];
-    
+
+    let ambiant_light = Color(1.0, 1.0, 0.0);
+
+    let light1 = Light{
+        location: Vector(-1.0, 0.0, 2.0),
+        diffuse_int: Color(0.2, 0.3, 0.3),
+        specular_int: Color(0.1, 0.5, 0.5)
+    };
+
+    let light2 = Light{
+        location: Vector(-0.7, 1.0, 3.0),
+        diffuse_int: Color(0.4, 0.7, 0.3),
+        specular_int: Color(0.5, 0.1, 0.8)
+    };
+
+    let lights = vec![light1, light2];
+
     let mut img = RgbImage::new(width, height);
 
     // Looping through each pixel in the 256x192 plain
     // and printing it's coordinates
     for x in 0..width {
         for y in 0..height {
-            let alpha = x as f32/(width - 1) as f32;
-            let beta = y as f32/(height -1) as f32;
+            let alpha = x as f32 / (width - 1) as f32;
+            let beta = y as f32 / (height - 1) as f32;
             let t = x2.lerp(&x1, alpha);
             let b = x4.lerp(&x3, alpha);
             let p = t.lerp(&b, beta);
             let d = p.sub(&c).norm();
-            
+
             let mut intersections = Vec::new();
 
             for sphere in &spheres {
@@ -59,21 +91,34 @@ fn main() {
                     intersections.push((t, sphere));
                 }
             }
-            if let Some ((_, closest_sphere)) = intersections.iter().min_by(|a,b| a.0.partial_cmp(&b.0).unwrap()) {
-                let color = closest_sphere.color;
-                img.put_pixel(x, y, Rgb([
-                    (color.0 * 255.0) as u8,
-                    (color.1 * 255.0) as u8,
-                    (color.2 * 255.0) as u8,
-                ]));
+            if let Some((t, closest_sphere)) = intersections
+                .iter()
+                .min_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
+            {   
+                let p_inter = closest_sphere.intersection_point(&c, &d, *t);
+
+                let surf_normal = closest_sphere.surf_normal(&p_inter);
+
+                for light in &lights {
+
+                }
+
+
+                let color = &closest_sphere.color;
+                img.put_pixel(
+                    x,
+                    y,
+                    Rgb([
+                        (color.0 * 255.0) as u8,
+                        (color.1 * 255.0) as u8,
+                        (color.2 * 255.0) as u8,
+                    ]),
+                );
             } else {
-                img.put_pixel(x, y, Rgb([0,0,0]));
+                img.put_pixel(x, y, Rgb([0, 0, 0]));
             }
         }
     }
 
     img.save("output.png").expect("Failed to save image")
 }
-
-
-
