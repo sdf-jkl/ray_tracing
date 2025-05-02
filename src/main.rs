3336333834bd8c1ef840950fe0af751b1806a16b
Konstantin.Tarasov <konstantin.tarasov@nyct.com>
2025-05-02 17:23:20 -0400

    Centered the ssaa grids

--- src/main.rs
+++ src/main.rs
@@ -63,8 +63,8 @@ fn main() {
             x4: Vector(-1.0, -0.75, 0.0),
         },
         camera: Vector(0.0, 0.0, -1.0),
-        width: 256,  //256
-        height: 192, //192
+        width: 256,  //256 1920
+        height: 192, //192 192
         ambient_light: Color(0.1, 0.1, 0.1),
         lights: vec![light1, light2],
         spheres: vec![sphere1, sphere2, sphere3],
